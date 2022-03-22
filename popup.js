b2b9ede0b8372e5546908d5d66accee0ece038df
willh-simpson <60246601+willh-simpson@users.noreply.github.com>
2022-03-22 15:18:40 -0400

    comment

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// connects to my personal reddit account to get posts
 let r = new snoowrap({
     userAgent: 'dynamic tts',
     clientId: 'd3eJD1-Na1mmQ9On_Z13VQ',
@@ -5,22 +6,26 @@ let r = new snoowrap({
     refreshToken: '43864152-vNRzhp9pvLRDmjtt8NCI2lxqz2Zy1w'
 });
 
+// just holds tab url
 let activeTab = "";
 
 function init() {
+    // everything needs to execute after the active tab has been found
     getCurrentTab().then(main);
 }
 
 function main() {
     activeTab = document.getElementById("output").innerHTML;
 
-    if (activeTab.includes("reddit.com/r/")) {
+    if (activeTab.includes("reddit.com/r/")) { // only works if you're viewing a submission
         if (activeTab.includes("/comments/")) {
             let thread = [];
             let submissionID = activeTab.split("/");
-            submissionID = submissionID[submissionID.indexOf("comments") + 1];
+            submissionID = submissionID[submissionID.indexOf("comments") + 1]; // post id comes after /comments/
 
+            // grabs replies only until the first loaded set on the page
             r.getSubmission(submissionID).expandReplies({limit: 1, depth: 1}).catch({ statusCode: 429 }, function() {}).then(s => {
+                // get author and comment text for each individual comment
                 s.comments.forEach(c => {
                     thread.push({ 
                         author: c.author.name, 
