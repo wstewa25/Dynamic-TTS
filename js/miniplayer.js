5e28a40c9909f54166afa4bfa32b31776c6fc92b
wstewa25 <78003757+wstewa25@users.noreply.github.com>
2022-04-13 23:06:04 -0400

    trying to get speed and volume buttons working properly

--- js/miniplayer.js
+++ js/miniplayer.js
@@ -8,18 +8,52 @@ window.onload = function() {
     volumePlus.addEventListener("click", function(){
         let volume = document.getElementById("volValue");
         volume.innerHTML = parseInt(volume.innerHTML) + 1;
+        editVolume(0.01);
     });
     volumeMinus.addEventListener("click", function(){
         let volume = document.getElementById("volValue");
         volume.innerHTML = parseInt(volume.innerHTML) - 1;
+        editVolume(-0.01);
     });
 
     speedPlus.addEventListener("click", function(){
         let speed = document.getElementById("speedValue");
-        speed.innerHTML = parseInt(speed.innerHTML) + 1;
+        switch (readSpeed.current) {
+            case 0.25:
+                editReadSpeed(0.25);
+            case 0.5:
+                editReadSpeed(0.5);
+            case 1.0:
+                    editReadSpeed(1);
+            case 2.0:
+                editReadSpeed(1);
+            case 3.0:
+                editReadSpeed(1);
+            case 4.0:
+                console.log("Max speed already reached");
+            default:
+                console.log("error in speedPlus");
+        }
+        speed.InnerHTML = readSpeed.current;
     });
     speedMinus.addEventListener("click", function(){
         let speed = document.getElementById("speedValue");
-        speed.innerHTML = parseInt(speed.innerHTML) - 1;
+        switch (readSpeed.current) {
+            case 0.25:
+                console.log("Min speed already reached");
+            case 0.5:
+                editReadSpeed(-0.25);
+            case 1.0:
+                editReadSpeed(-0.5);
+            case 2.0:
+                editReadSpeed(-1);
+            case 3.0:
+                editReadSpeed(-1);
+            case 4.0:
+                editReadSpeed(-1);
+            default:
+                console.log("error in speedMinus");
+        }
+        speed.InnerHTML = readSpeed.current;
     });
 }
